a873596a4b7687ce10dc29a4674695d13cb55121
Zhivko Shopov <zhishopov@gmail.com>
2025-09-30 01:39:46 +0100

    Install libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
+import "./styles.css";
+
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://squadbuilder.online/health", {
+        credentials: "include",
+      });
+      const result = await response.json();
+      console.log(result);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
-      <h1>Hello</h1>
+      <h1 className="text-red-500">Hello</h1>
+      <img src="logo2.webp" alt="" />
     </>
   );
 }
