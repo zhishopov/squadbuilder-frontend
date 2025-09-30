5634cbf83fc4d86aced0368e7dc4d160f8aea820
Zhivko Shopov <zhishopov@gmail.com>
2025-09-30 18:29:54 +0100

    Wrap App with Redux Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
