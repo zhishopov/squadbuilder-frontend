1727f271ddba4287951a0657ee27ac9c6b9945d5
Zhivko Shopov <zhishopov@gmail.com>
2025-10-08 18:14:53 +0100

    Add MembersCard component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header";
+import MembersCard from "../features/dashboard/components/MembersCard";
 import SquadCard from "../features/dashboard/components/SquadCard";
 
 export default function Dashboard() {
@@ -8,6 +9,7 @@ export default function Dashboard() {
       <main className="mx-auto max-w-5xl px-4 py-6">
         <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
         <SquadCard></SquadCard>
+        <MembersCard></MembersCard>
       </main>
     </>
   );
