3f053b3ac69fe98d6e30a226948e6d767c309489
Zhivko Shopov <zhishopov@gmail.com>
2025-10-26 18:07:26 +0000

    Update api setup to show error toasts for failed requests

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { showErrorToast } from "./error";
 
 const envBaseUrl = import.meta.env.VITE_API_URL;
 const fallbackBaseUrl =
@@ -8,18 +9,34 @@ const fallbackBaseUrl =
 
 const baseUrl = envBaseUrl ?? fallbackBaseUrl;
 
+const baseQuery = fetchBaseQuery({
+  baseUrl,
+  credentials: "include",
+  prepareHeaders: (headers) => {
+    if (!headers.has("Content-Type")) {
+      headers.set("Content-Type", "application/json");
+    }
+    headers.set("Accept", "application/json");
+    return headers;
+  },
+});
+
+const baseQueryWithErrorHandling: typeof baseQuery = async (
+  args,
+  api,
+  extraOptions
+) => {
+  const result = await baseQuery(args, api, extraOptions);
+
+  if (result.error) {
+    showErrorToast(result.error);
+  }
+
+  return result;
+};
+
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({
-    baseUrl,
-    credentials: "include",
-    prepareHeaders: (headers) => {
-      if (!headers.has("Content-Type")) {
-        headers.set("Content-Type", "application/json");
-      }
-      headers.set("Accept", "application/json");
-      return headers;
-    },
-  }),
+  baseQuery: baseQueryWithErrorHandling,
   endpoints: () => ({}),
 });
